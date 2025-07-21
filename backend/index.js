86906ca03eadf0f6be95eaa10de460a1fa0b8a05
Tirthchavda <tirth7859@gmail.com>
2025-07-21 23:31:36 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -38,15 +38,4 @@ app.listen(PORT,()=>{
     connectDB(); // Ensure connectDB logs "mongodb connected successfully" inside its implementation
     console.log(`Server running at port ${PORT}`);
   
-    
-
-    //this is free work
-    console.log("Free work done by @sachinmehta_");
-
-
-
-
-
-
-
 })
