3441abe13f7b5b00e344c499b32a79d8ff8c64af
Tirthchavda <tirth7859@gmail.com>
2025-07-26 00:49:26 +0530

    ok

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route("/post").post(isAuthenticated, postjob);
 router.route("/get").get(isAuthenticated, getAlljobs);
-router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
-router.route("/get/:id").post(isAuthenticated, getJobById);
+router.route("/getadminjobs").(isAuthenticated, getAdminJobs);
+router.route("/get/:id").get(isAuthenticated, getJobById);
 
 export default router;
